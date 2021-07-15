1f49d5eaa8c86bd476f35c30b39c9d41e7f6e5b8
Maggie McDonald <maggiegreymcdonald@gmail.com>
2021-07-15 13:53:56 -0400

    Added <Head> to first-post.js
    Added Head and title to first-post.js.

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,8 +1,12 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default function firstPost() {
     return (
         <div>
+            <Head>
+                <title>First Post</title>
+            </Head>
             <h1>Here's me post!</h1>
             <h2>
                 <Link href="\">
