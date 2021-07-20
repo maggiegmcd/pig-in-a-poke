df60776d2c1d1a22b1ba87c346f17b7a0ec65e8d
Maggie McDonald <maggiegreymcdonald@gmail.com>
2021-07-20 12:32:46 -0400

    Updated site title

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Maggie'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Head Hog Honcho\'s Hideout'
 
 export default function Layout({ children, home }) {
     return (
